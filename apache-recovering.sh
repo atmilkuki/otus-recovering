def872a0622a6dc7ee167e971805df8c3177c99b
atmilkuki <154456334+atmilkuki@users.noreply.github.com>
2024-02-26 20:37:54 +0500

    Create apache-recovering.sh

---
+++ apache-recovering.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#Configuring network
+cp /etc/netplan/00-installer-config.yaml /etc/netplan/00-installer-config.yaml.bak
+cp ./apache-network /etc/netplan/00-installer-config.yaml
+netplan apply
+
+#Installing and configuring Apache
+apt install apache2 -y
+systemctl enable apache2.service
+cp ./apache-both-ports-config /etc/apache2/ports.conf
+service apache2 restart
+
+#Installing and configuring MySQL server
+apt install mysql-server -y
+systemctl enable mysql.service
+cp ./apache-mysql-config /etc/mysql-somewhere
+#commands to create user for replica
+
+#Installing and configuring agents
+apt install prometheus-node-exporter prometheus-apache-exporter prometheus-mysqld-exporter
+systemctl enable --now prometheus-node-exporter
+systemctl enable --now prometheus-apache-exporter
+systemctl enable --now prometheus-mysqld-exporter
+
+dpkg -i https://cdn.otus.ru/media/public/65/6d/filebeat_8.9.1_amd64-224190-656d53.deb
+cp /etc/filebeat/filebeat.yml /etc/filebeat/filebeat.yml.bak
+cp ./apache-both-fbeats-config /etc/filebeat/filebeat.yml
+systemctl enable --now filebeat.service
